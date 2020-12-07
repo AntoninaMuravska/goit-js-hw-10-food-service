93472d56f7911b623cc7b254eec7407a9ce5bcd5
Antonina Muravska <at.tonya@gmail.com>
2020-12-07 09:47:56 +0200

    fix changeColorTheme function

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -11,8 +11,6 @@ const Theme = {
 console.log(localStorage);
 console.dir(refs.switch);
 refs.switch.addEventListener('change', changeColorTheme);
-refs.switch.addEventListener('change', setLocalTheme);
-
 // console.dir(refs.switch);
 
 function changeColorTheme() {
@@ -20,24 +18,12 @@ function changeColorTheme() {
     if (checked) {
         refs.body.classList.add(Theme.DARK);
         refs.body.classList.remove(Theme.LIGHT);
-    } else {
-         refs.body.classList.add(Theme.LIGHT);
-         refs.body.classList.remove(Theme.DARK);
-    }   
-}
-
-// сохраняем тему в localStorage
-function setLocalTheme() {
-    const checked = refs.switch.checked;
-        // console.dir(localTheme);
-
-    if (checked) {
         localStorage.setItem('theme', Theme.DARK);
     } else {
-        localStorage.removeItem('theme');
+        refs.body.classList.add(Theme.LIGHT);
+        refs.body.classList.remove(Theme.DARK);
         localStorage.setItem('theme', Theme.LIGHT);
-    }
-    
+    }   
 }
 
 // Добавляем класс на боди и перемещаем переключатель, если в localStorage была темная тема
