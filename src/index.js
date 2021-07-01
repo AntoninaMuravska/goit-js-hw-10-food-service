1d8181d5d9ebea728d5b5891c2cac970d52dcf4b
Antonina Muravska <at.tonya@gmail.com>
2021-07-01 20:19:48 +0300

    init parcel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-
-import dishes from './menu.json';
-import itemTemplate from './templates/dishes-cards.hbs';
-import './js/change-theme.js';
-import './styles.css';
-
-const menuRef = document.querySelector('.js-menu');
-
-const murkUp = itemTemplate(dishes);
-
-menuRef.insertAdjacentHTML('beforeend', murkUp);
+import './sass/main.scss';
